f8dd6b7cc6f94f60857e14bc40d9a48525cd1556
RichDobrz <rjdobrz@gmail.com>
2019-09-16 15:29:32 -0400

    functional react app

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+ 
+ const Header = () => {
+  return (
+    <header className="App-header">
+    <p>Classic BIS List</p>
+    <a href="#" data-wowhead="item=2828"></a>
+  </header>
+  )
+ }
+
+export default Header
