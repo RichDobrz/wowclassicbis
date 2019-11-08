89a501ef21229012a8df33dec3359807e89a5f8b
RichDobrz <rjdobrz@gmail.com>
2019-11-08 13:20:06 -0500

    basic class setup

--- client/src/wowhead/classData.js
+++ client/src/wowhead/classData.js
@@ -1,6 +1,87 @@
 const classWork = {
-    "Warrior":{
-        "head": "item"
+    
+    Warrior:{
+        fury: {
+            head: "item"
+        },
+        protection: {
+            head: "item"
+        }
+    },
+    
+    Druid: {
+        balance: {
+            head: "item"
+        },
+        feral: {
+            head: "item"
+        },
+        tank: {
+            head: "item"
+        },
+        resto: {
+            head: "item"
+        }
+    },
+    
+    Rogue: {
+        sword: {
+            head: "item"
+        },
+        dagger: {
+            head: "item"
+        } 
+    },
+    
+    Shaman: {
+        elemental: {
+            head: "item"
+        },
+        enhance: {
+            head: "item"
+        },
+        resto: {
+            head: "item"
+        }
+    },
+    
+    Hunter: {
+        marksman: {
+            item: "item"
+        }
+    },
+    
+    Priest: {
+        shadow: {
+            head: "item"
+        },
+        holy: {
+            head: "item"
+        }
+    },
+    
+    Paladin: {
+        holy: {
+            head: "item"
+        },
+        protection: {
+            head: "item"
+        },
+        retribution: {
+            head: "item"
+        },
+    },
+    
+    Mage: {
+        frost: {
+            head: "item"
+        }
+    },
+    
+    Warlock: {
+        affliction: {
+            head: "item"
+        }
     }
 }
 
